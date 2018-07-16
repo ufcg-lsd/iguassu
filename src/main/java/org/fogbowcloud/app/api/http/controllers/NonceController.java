01f9d20d4d56ec2c28f465306bd6d4b6931633bf
Italo Batista <italohmb@gmail.com>
2018-07-16 13:52:56 -0300

    Implement endpoint for requesting nonce

---
+++ src/main/java/org/fogbowcloud/app/api/http/controllers/NonceController.java
@@ -0,0 +1,27 @@
+package org.fogbowcloud.app.api.http.controllers;
+
+import org.fogbowcloud.app.ArrebolController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = NonceController.NONCE_ENDPOINT)
+public class NonceController {
+
+    public static final String NONCE_ENDPOINT = "nonce";
+
+    @Lazy
+    @Autowired
+    ArrebolController arrebolController;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<String> getNonce() {
+        int nonce = this.arrebolController.getNonce();
+        String nonceStr = String.valueOf(nonce);
+        return new ResponseEntity<String>(nonceStr, HttpStatus.OK);
+    }
+}
