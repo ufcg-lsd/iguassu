ec5bb24557298bdd59b552eb874da8f5c9bc3dd2
Italo Batista <italohmb@gmail.com>
2018-07-16 13:28:35 -0300

    Handling errors when processing requests and returning appropriate status

---
+++ src/main/java/org/fogbowcloud/app/exception/InvalidParameterException.java
@@ -0,0 +1,19 @@
+package org.fogbowcloud.app.exception;
+
+public class InvalidParameterException extends ArrebolException {
+
+    private static final String DEFAULT_MESSAGE = "Invalid parameter exception";
+
+    public InvalidParameterException() {
+        super(DEFAULT_MESSAGE);
+    }
+
+    public InvalidParameterException(String message) {
+        super(message);
+    }
+
+    public InvalidParameterException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
