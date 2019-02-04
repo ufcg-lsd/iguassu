de42a1fad113ccc1571050538c26fb29cd605052
Emanuel Martins <emanueljoivo@gmail.com>
2019-02-04 15:46:25 -0300

    Updating tests of TestAsyncJobBuilder

--- src/test/java/org/fogbowcloud/app/TestAsyncJobBuilder.java
+++ src/test/java/org/fogbowcloud/app/TestAsyncJobBuilder.java
@@ -1,12 +1,14 @@
 package org.fogbowcloud.app;
 
 import org.fogbowcloud.app.datastore.JobDataStore;
+import org.fogbowcloud.app.datastore.OAuthTokenDataStore;
 import org.fogbowcloud.app.exception.IguassuException;
 import org.fogbowcloud.app.exception.NameAlreadyInUseException;
 import org.fogbowcloud.app.jdfcompiler.main.CompilerException;
 import org.fogbowcloud.app.model.JDFJob;
 import org.fogbowcloud.app.model.LDAPUser;
 import org.fogbowcloud.blowout.core.BlowoutController;
+import org.fogbowcloud.blowout.core.constants.AppPropertiesConstants;
 import org.fogbowcloud.blowout.core.exception.BlowoutException;
 import org.fogbowcloud.blowout.core.model.Task;
 import org.junit.Assert;
@@ -22,31 +24,36 @@ import java.util.Properties;
 public class TestAsyncJobBuilder {
 
     private static final String EXSIMPLE_JOB = "test" + File.separator + "resources" + File.separator + "SimpleJob2.jdf";
-    private static final String ARREBOL_CONF = "test" + File.separator + "arrebol.conf";
-    private static final String BLOWOUT_CONF = "test" + File.separator + "blowout.conf";
+    private static final String IGUASSU_CONF = "iguassu.conf";
+    private static final String BLOWOUT_CONF = "sched.conf";
 
     private static final String user = "arrebolservice";
     private static final String username = "arrebolservice";
 
-    private IguassuController arrebol;
+    private IguassuController iguassuController;
     private BlowoutController blowout;
+    private OAuthTokenDataStore oAuthTokenDataStore;
 
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
         try {
             Properties properties = new Properties();
-            properties.load(new FileInputStream(ARREBOL_CONF));
+            properties.load(new FileInputStream(IGUASSU_CONF));
             properties.load(new FileInputStream(BLOWOUT_CONF));
-            arrebol = Mockito.spy(new IguassuController(properties));
+            iguassuController = Mockito.spy(new IguassuController(properties));
         } catch (BlowoutException | IguassuException | IOException e) {
             e.printStackTrace();
         }
 
         JobDataStore dataStore = Mockito.spy(new JobDataStore("jdbc:h2:/tmp/datastores/testfogbowresourcesdatastore"));
-        arrebol.setDataStore(dataStore);
+        iguassuController.setDataStore(dataStore);
 
         blowout = Mockito.mock(BlowoutController.class);
-        arrebol.setBlowoutController(blowout);
+        iguassuController.setBlowoutController(blowout);
+
+        oAuthTokenDataStore = Mockito.mock(OAuthTokenDataStore.class);
+
+        iguassuController.init();
     }
 
     @Test
@@ -54,15 +61,15 @@ public class TestAsyncJobBuilder {
         try {
             Mockito.doNothing().when(blowout).addTaskList(Mockito.anyListOf(Task.class));
 
-            String id = arrebol.addJob(EXSIMPLE_JOB, new LDAPUser(user, username));
+            String id = iguassuController.addJob(EXSIMPLE_JOB, new LDAPUser(user, username));
 
-            JDFJob job = arrebol.getJobById(id, user);
+            JDFJob job = iguassuController.getJobById(id, user);
             Assert.assertEquals(JDFJob.JDFJobState.SUBMITTED, job.getState());
             Assert.assertEquals(0, job.getTasks().size());
 
-            arrebol.waitForJobCreation(job.getId());
+            iguassuController.waitForJobCreation(job.getId());
 
-            job = arrebol.getJobById(id, user);
+            job = iguassuController.getJobById(id, user);
             Assert.assertEquals(JDFJob.JDFJobState.CREATED, job.getState());
             Assert.assertEquals(3, job.getTasks().size());
         } catch (CompilerException | NameAlreadyInUseException | BlowoutException | IOException | InterruptedException e) {
@@ -76,15 +83,15 @@ public class TestAsyncJobBuilder {
         try {
             Mockito.doNothing().when(blowout).addTaskList(Mockito.anyListOf(Task.class));
 
-            String id = arrebol.addJob(EXSIMPLE_JOB, new LDAPUser(user, username));
+            String id = iguassuController.addJob(EXSIMPLE_JOB, new LDAPUser(user, username));
 
-            JDFJob job = arrebol.getJobById(id, user);
+            JDFJob job = iguassuController.getJobById(id, user);
             Assert.assertEquals(JDFJob.JDFJobState.SUBMITTED, job.getState());
             Assert.assertEquals(0, job.getTasks().size());
 
-            arrebol.stopJob(id, user);
+            iguassuController.stopJob(id, user);
 
-            job = arrebol.getJobById(id, user);
+            job = iguassuController.getJobById(id, user);
             Assert.assertNull(job);
         } catch (CompilerException | NameAlreadyInUseException | BlowoutException | IOException e) {
             e.printStackTrace();
