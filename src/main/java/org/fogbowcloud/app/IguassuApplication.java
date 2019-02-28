d451dbb1f9520e6845e7757b546f6c0a1d723763
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-02-28 10:07:00 -0300

    Refactored IguassuMainRunner

--- src/main/java/org/fogbowcloud/app/IguassuApplication.java
+++ src/main/java/org/fogbowcloud/app/IguassuApplication.java
@@ -1,20 +1,14 @@
 package org.fogbowcloud.app;
 
 import org.fogbowcloud.app.core.IguassuController;
-import org.fogbowcloud.app.core.constants.IguassuPropertiesConstants;
-import org.fogbowcloud.app.core.constants.IguassuGeneralConstants;
 import org.fogbowcloud.app.core.exceptions.IguassuException;
 import org.fogbowcloud.blowout.core.exception.BlowoutException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 
-import java.io.FileInputStream;
 import java.util.Properties;
 
 @SpringBootApplication
@@ -45,45 +39,4 @@ public class IguassuApplication {
         SpringApplication.run(IguassuApplication.class, args);
     }
 
-    public class IguassuMainRunner implements CommandLineRunner, ApplicationListener<EmbeddedServletContainerInitializedEvent> {
-
-        @Autowired
-        private Properties properties;
-
-        @Lazy
-        @Autowired
-        IguassuController iguassuController;
-
-        @Override
-        public void run(String...args) {
-
-            String iguassuConfPath;
-            String schedConfPath;
-
-            if (args.length > 0) {
-                iguassuConfPath = args[0];
-                schedConfPath = args[1];
-            } else {
-                iguassuConfPath = IguassuGeneralConstants.DEFAULT_IGUASSU_CONF_FILE_PATH;
-                schedConfPath = IguassuGeneralConstants.DEFAULT_SCHED_CONF_FILE_PATH;
-            }
-
-            loadProperties(iguassuConfPath, schedConfPath);
-        }
-
-        @Override
-        public void onApplicationEvent(final EmbeddedServletContainerInitializedEvent event) {
-            int port = event.getEmbeddedServletContainer().getPort();
-            properties.setProperty(IguassuPropertiesConstants.REST_SERVER_PORT, String.valueOf(port));
-        }
-
-        private void loadProperties(String iguassuConfPath, String schedConfPath){
-            try {
-                properties.load(new FileInputStream(iguassuConfPath));
-                properties.load(new FileInputStream(schedConfPath));
-            } catch (Exception e){
-                System.exit(1);
-            }
-        }
-    }
 }
