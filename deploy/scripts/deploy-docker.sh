7ca3dbd4f2d023cfbdb23b1758ecb6358f630204
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-03-21 14:25:04 -0300

    Created deploy.conf and Fixed sending the key

--- deploy/scripts/deploy-docker.sh
+++ deploy/scripts/deploy-docker.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
-DIR_PATH=$(pwd)
-CONF_FILES_DIR="conf-files"
+MY_PATH="`dirname \"$0\"`"              # relative
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
+if [ -z "$MY_PATH" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
 
-IMAGE_NAME="ufcglsd/iguassu:latest"
-CONTAINER_NAME="iguassu"
+echo "Dir path: "$MY_PATH
 
-MANAGER_FRONT_PORT="9000"
-MANAGER_BACK_PORT="8080"
-FRONTEND_PORT="9000"
-BACKEND_PORT="8080"
+DEPLOY_CONF_FILE=$MY_PATH"/deploy.conf"
+echo "deploy.conf file path:" $DEPLOY_CONF_FILE
+
+IMAGE_NAME_PATTERN="image_name"
+IMAGE_NAME=$(grep $IMAGE_NAME_PATTERN $DEPLOY_CONF_FILE | awk -F "=" '{print $2}')
+echo "Image Name: " $IMAGE_NAME
+
+CONTAINER_NAME_PATTERN="container_name"
+CONTAINER_NAME=$(grep $CONTAINER_NAME_PATTERN $DEPLOY_CONF_FILE | awk -F "=" '{print $2}')
+echo "Container Name: " $CONTAINER_NAME
+
+MANAGER_BACK_PORT_PATTERN="manager_back_port"
+MANAGER_BACK_PORT=$(grep $MANAGER_BACK_PORT_PATTERN $DEPLOY_CONF_FILE | awk -F "=" '{print $2}')
+echo "Manager Back Port: " $MANAGER_BACK_PORT
+
+MANAGER_FRONT_PORT_PATTERN="manager_front_port"
+MANAGER_FRONT_PORT=$(grep $MANAGER_FRONT_PORT_PATTERN $DEPLOY_CONF_FILE | awk -F "=" '{print $2}')
+echo "Manager front port: " $MANAGER_FRONT_PORT
+
+readonly CONF_FILES_DIR="conf-files"
+readonly FRONTEND_PORT="9000"
+readonly BACKEND_PORT="8080"
 
 PRIVATE_KEY_FILE="fogbow_priv"
 
@@ -23,6 +45,9 @@ sudo docker run -idt \
     -p $MANAGER_BACK_PORT:$BACKEND_PORT \
     $IMAGE_NAME
 
+sudo docker exec -d $CONTAINER_NAME /bin/bash -c 'mkdir .ssh'
+
+sudo docker cp $DIR_PATH/$CONF_FILES_DIR/secret/. $CONTAINER_NAME:/root/.ssh
 sudo docker cp $DIR_PATH/$CONF_FILES_DIR/backend-confs/. $CONTAINER_NAME:/root/iguassu
 sudo docker cp $DIR_PATH/$CONF_FILES_DIR/frontend-confs/. $CONTAINER_NAME:/root/iguassu-dashboard/app
 
