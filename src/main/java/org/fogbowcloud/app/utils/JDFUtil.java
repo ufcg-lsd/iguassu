df0bedfe095a0441df6da8a084f4626a48c64d0e
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-06-26 11:43:40 -0300

    Removed empty spaces before equals

---
+++ src/main/java/org/fogbowcloud/app/utils/JDFUtil.java
@@ -0,0 +1,20 @@
+package org.fogbowcloud.app.utils;
+
+import org.fogbowcloud.app.jdfcompiler.job.JobSpecification;
+import org.fogbowcloud.app.jdfcompiler.job.TaskSpecification;
+import org.fogbowcloud.app.jdfcompiler.semantic.JDLCommand;
+import org.fogbowcloud.app.jdfcompiler.semantic.RemoteCommand;
+
+public class JDFUtil {
+
+    public static void removeEmptySpaceFromVariables(JobSpecification jobSpec) {
+        for(TaskSpecification taskSpec : jobSpec.getTaskSpecs()){
+            for(JDLCommand command : taskSpec.getTaskBlocks()){
+                if(command instanceof RemoteCommand){
+                    String content = ((RemoteCommand) command).getContent().replaceAll(" =", "=");
+                    ((RemoteCommand) command).setContent(content);
+                }
+            }
+        }
+    }
+}
