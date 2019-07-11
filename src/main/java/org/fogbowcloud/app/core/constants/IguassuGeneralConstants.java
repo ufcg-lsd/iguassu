0afef4b3d4ceaf05db61f22889a8b6110de666d3
Emanuel Joívo <emanueljoivo@gmail.com>
2019-07-11 09:56:52 -0300

    Updates configuration file properties

--- src/main/java/org/fogbowcloud/app/core/constants/IguassuGeneralConstants.java
+++
@@ -1,6 +0,0 @@
-package org.fogbowcloud.app.core.constants;
-
-public class IguassuGeneralConstants {
-    public static final String DEFAULT_IGUASSU_CONF_FILE_PATH = "iguassu.conf";
-    public static final String DB_DATASTORE_URL = "datastore_url";
-}
