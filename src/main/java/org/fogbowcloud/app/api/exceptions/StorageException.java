78913dd682d4d1c60b94ff2c0f3e6c5ac815517e
Joivo <emanueljoivo@gmail.com>
2019-07-29 18:18:52 -0300

    Deleted unused exception and docs some others
    Exceptions javadocs.

--- src/main/java/org/fogbowcloud/app/api/exceptions/StorageException.java
+++ src/main/java/org/fogbowcloud/app/api/exceptions/StorageException.java
@@ -1,5 +1,6 @@
 package org.fogbowcloud.app.api.exceptions;
 
+/** Appropriate exception for when any data storage access operation fails. */
 public class StorageException extends RuntimeException {
 
     public StorageException(String message) {
