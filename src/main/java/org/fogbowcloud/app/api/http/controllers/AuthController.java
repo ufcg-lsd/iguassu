ff05f72232130fcc7ecc8ca8f46f1c14cc747d3e
Joivo <emanueljoivo@gmail.com>
2019-07-29 19:44:46 -0300

    Added docs for AuthManager
    Describing your operations and responsibility.

--- src/main/java/org/fogbowcloud/app/api/http/controllers/AuthController.java
+++ src/main/java/org/fogbowcloud/app/api/http/controllers/AuthController.java
@@ -3,6 +3,7 @@ package org.fogbowcloud.app.api.http.controllers;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import java.security.GeneralSecurityException;
 import org.apache.log4j.Logger;
 import org.fogbowcloud.app.api.constants.Documentation;
 import org.fogbowcloud.app.api.exceptions.StorageServiceConnectException;
@@ -13,9 +14,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.security.GeneralSecurityException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = Documentation.Endpoint.AUTH)
