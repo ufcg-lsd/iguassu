c8f8a7c4326b410d9225c8738b92b400ae4551ec
Joivo <emanueljoivo@gmail.com>
2019-08-10 20:22:47 -0300

    Fixed run script

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-./iguass/bin/start-iguassu-service.sh
-./arrebol/bin/start-service.sh
+../
+
+./iguass/bin/start-iguassu-service.sh &
+./arrebol/bin/start-service.sh &
 
 yarn run serve --port 8082
