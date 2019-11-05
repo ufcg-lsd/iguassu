54a18bb4daeb9c00ede9c399bca6689dc0ea0bad
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-11-05 16:45:57 -0300

    Renamed fields from QueueDTO

--- src/main/java/org/fogbowcloud/app/jes/arrebol/dtos/QueueDTO.java
+++ src/main/java/org/fogbowcloud/app/jes/arrebol/dtos/QueueDTO.java
@@ -1,24 +1,28 @@
 package org.fogbowcloud.app.jes.arrebol.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.gson.annotations.SerializedName;
 
 public class QueueDTO {
 
     public String id;
     public String name;
+    @JsonProperty("waiting_jobs")
     @SerializedName("waiting_jobs")
     public long waitingJobs;
-    @SerializedName("workers_nodes")
-    public Integer workersNodes;
-    @SerializedName("worker_pool")
-    public Integer workerPool;
+    @JsonProperty("worker_pools")
+    @SerializedName("worker_pools")
+    public Integer workerPools;
+    @JsonProperty("pools_size")
+    @SerializedName("pools_size")
+    public Integer poolsSize;
 
-    public QueueDTO(String id, String name, long waitingJobs, int workersNodes, int workerPool) {
+    public QueueDTO(String id, String name, long waitingJobs, int workerPools, int poolsSize) {
         this.id = id;
         this.name = name;
         this.waitingJobs = waitingJobs;
-        this.workersNodes = workersNodes;
-        this.workerPool = workerPool;
+        this.workerPools = workerPools;
+        this.poolsSize = poolsSize;
     }
 
     public String getId() {
@@ -33,11 +37,11 @@ public class QueueDTO {
         return waitingJobs;
     }
 
-    public Integer getWorkersNodes() {
-        return workersNodes;
+    public Integer getWorkerPools() {
+        return workerPools;
     }
 
-    public Integer getWorkerPool() {
-        return workerPool;
+    public Integer getPoolsSize() {
+        return poolsSize;
     }
 }
