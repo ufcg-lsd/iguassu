73fce55bba949587b9e38c5da4b14c7643b348b8
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-11-21 10:27:39 -0300

    Created addNode and containsPool method to Provisioning Request Helper

--- src/main/java/org/fogbowcloud/app/ps/Constants.java
+++ src/main/java/org/fogbowcloud/app/ps/Constants.java
@@ -4,6 +4,8 @@ public class Constants {
     public static final String POOL_NAME_KEY = "name";
     public static final String ERROR_MSG_KEY = "error";
     public static final String MSG_KEY = "msg";
+    public static final String PROVIDER_KEY = "provider";
+    public static final String ADDRESS_KEY = "ip";
 
     public static final class Endpoint {
         public static final String POOLS = "/pools";
--- src/main/java/org/fogbowcloud/app/ps/ProvisioningRequestHelper.java
+++ src/main/java/org/fogbowcloud/app/ps/ProvisioningRequestHelper.java
@@ -2,6 +2,9 @@ package org.fogbowcloud.app.ps;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.Map;
 import java.util.Properties;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
@@ -24,7 +27,7 @@ public class ProvisioningRequestHelper {
     }
 
     public void createPool(String poolName) throws Exception {
-        String url = String.format(Endpoint.POOL, poolName);
+        String url = String.format(serviceBaseUrl + Endpoint.POOL, poolName);
 
         JsonObject poolBody = new JsonObject();
         poolBody.addProperty(Constants.POOL_NAME_KEY, poolName);
@@ -36,10 +39,35 @@ public class ProvisioningRequestHelper {
     }
 
     public Pool getPool(String poolName) throws Exception {
-        String url = String.format(Endpoint.POOL, poolName);
+        String url = String.format(serviceBaseUrl + Endpoint.POOL, poolName);
         String jsonResponse = HttpWrapper.doRequest(HttpGet.METHOD_NAME, url, null);
         Pool pool = this.jsonUtil.fromJson(jsonResponse, Pool.class);
         return pool;
+    }
+
+    public void addNode(String poolName, String nodeAddress) throws Exception {
+        String url = String.format(serviceBaseUrl, Endpoint.POOL, poolName);
+
+        JsonObject nodeBody = new JsonObject();
+        nodeBody.addProperty(Constants.PROVIDER_KEY, "ansible");
+        nodeBody.addProperty(Constants.ADDRESS_KEY, nodeAddress);
+        StringEntity body = new StringEntity(nodeBody.toString());
+
+        String jsonResponse = HttpWrapper.doRequest(HttpPost.METHOD_NAME, url, null, body);
+        JsonObject response = this.jsonUtil.fromJson(jsonResponse, JsonObject.class);
+        logger.info("Response message: " + response.get(Constants.MSG_KEY));
+    }
+
+    public boolean containsPool(String poolName) throws Exception {
+        boolean answer = false;
+        String url = serviceBaseUrl + Endpoint.POOLS;
+        Type type = new TypeToken<Map<String, Pool>>() {}.getType();
+        String jsonResponse = HttpWrapper.doRequest(HttpGet.METHOD_NAME, url, null);
+        Map<String, Pool> pools = this.jsonUtil.fromJson(jsonResponse, type);
+        if (pools.containsKey(poolName)) {
+            answer = true;
+        }
+        return answer;
     }
 
 }
