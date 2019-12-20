82da3ece999c14a1685bdb80d2ae19dac1a1e3de
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-12-20 16:55:55 -0300

    Added id and changed list nodes to map on Pool

--- src/main/java/org/fogbowcloud/app/ps/models/Pool.java
+++ src/main/java/org/fogbowcloud/app/ps/models/Pool.java
@@ -1,32 +1,39 @@
 package org.fogbowcloud.app.ps.models;
 
 import com.google.gson.annotations.SerializedName;
-import java.util.List;
+import java.util.Map;
 
 public class Pool {
+
+    private String id;
 
     @SerializedName("name")
     private String poolName;
 
     @SerializedName("nodes")
-    private List<Node> nodes;
+    private Map<String, Node> nodes;
+
+    public String getId() {
+        return id;
+    }
 
     public String getPoolName() {
         return poolName;
     }
 
-    public List<Node> getNodes() {
+    public Map<String, Node> getNodes() {
         return nodes;
     }
 
     public Node getNode(String nodeAddress) {
-        Node node = null;
-        for(Node n : nodes) {
-            if(n.getAddress().equals(nodeAddress)) {
-                node = n;
-                break;
-            }
-        }
-        return node;
+//        Node node = null;
+//        for(Node n : nodes) {
+//            if(n.getAddress().equals(nodeAddress)) {
+//                node = n;
+//                break;
+//            }
+//        }
+//        return node;
+        return null;
     }
 }
