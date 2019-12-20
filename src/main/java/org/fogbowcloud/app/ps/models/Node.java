3dee2813712ed22c774b5a414f1367503b0eb166
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-12-20 14:42:10 -0300

    Updated fileds from ps node

--- src/main/java/org/fogbowcloud/app/ps/models/Node.java
+++ src/main/java/org/fogbowcloud/app/ps/models/Node.java
@@ -3,14 +3,18 @@ package org.fogbowcloud.app.ps.models;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.gson.annotations.SerializedName;
+import java.util.Map;
 
 public class Node {
 
-    @JsonProperty("resource_address")
+    private String driver;
+
+    private Map<String, String> spec;
+
     @SerializedName("ip")
     private String address;
 
-    @SerializedName("state")
+//    @SerializedName("state")
     private String state;
 
     public String getAddress() {
@@ -22,12 +26,12 @@ public class Node {
     }
 
     @JsonIgnore
-    public boolean isProvisioned() {
-        return state.equals("provisioned");
+    public boolean isReady() {
+        return state.equals("READY");
     }
 
     @JsonIgnore
     public boolean isFailed() {
-        return state.equals("failed");
+        return state.equals("FAILED");
     }
 }
